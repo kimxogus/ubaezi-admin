c32d2443915481c35978bb667e41b8a833a37868
kimxogus <kgyoo8232@gmail.com>
2017-08-29 23:45:36 +0900

    add create handler

--- functions/index.js
+++ functions/index.js
@@ -8,10 +8,11 @@ const database = admin.database();
 
 exports.onMenuWrite = functions.database.ref('/menus/{id}').onWrite(event => {
   const { params: { id } } = event;
-  const data = event.data.val();
+  const eventSnapshot = event.data;
+  const data = eventSnapshot.val();
   const storeRefPath = `/stores/${data.storeId}/menus/${id}`;
   const menuGroupRefPath = `/menuGroups/${data.menuGroupId}/menus/${id}`;
-  if (!event.data.exists()) {
+  if (!eventSnapshot.exists()) {
     // on delete
     const updates = {
       [storeRefPath]: null,
@@ -20,11 +21,13 @@ exports.onMenuWrite = functions.database.ref('/menus/{id}').onWrite(event => {
     database.ref().update(updates);
   } else {
     // on create or update
-    const updates = {
-      [storeRefPath]: true,
-      [menuGroupRefPath]: true,
-    };
-    database.ref().update(updates);
+    if (!eventSnapshot.previous.exists()) {
+      const updates = {
+        [storeRefPath]: true,
+        [menuGroupRefPath]: true,
+      };
+      database.ref().update(updates);
+    }
   }
 });
 
@@ -32,11 +35,12 @@ exports.onMenuGroupWrite = functions.database
   .ref('/menuGroups/{id}')
   .onWrite(event => {
     const { params: { id } } = event;
-    const data = event.data.val();
+    const eventSnapshot = event.data;
+    const data = eventSnapshot.val();
     const storeRefPath = `/stores/${data.storeId}/menuGroups/${id}`;
-    if (!event.data.exists()) {
+    if (!eventSnapshot.exists()) {
       // on delete
-      const previous = event.data.previous.val();
+      const previous = eventSnapshot.previous.val();
 
       const menuUpdates = Object.keys(previous.menus).reduce((a, b) => {
         a[`/menus/${b}`] = null;
@@ -48,20 +52,33 @@ exports.onMenuGroupWrite = functions.database
         .update(Object.assign({ [storeRefPath]: null }, menuUpdates));
     } else {
       // on create or update
-      const updates = {
-        [storeRefPath]: true,
-        [`/menuGroups/${id}/menuCount`]: Object.keys(data.menus).length,
-      };
-      database.ref().update(updates);
+      const updates = {};
+
+      if (!eventSnapshot.previous.exists()) {
+        updates[`/menuGroups/${id}/menus`] = data.menus || {};
+      }
+
+      if (!eventSnapshot.previous.exists()) {
+        updates[storeRefPath] = true;
+      }
+
+      if (eventSnapshot.child('menus').changed()) {
+        updates[`/menuGroups/${id}/menuCount`] = Object.keys(data.menus).length;
+      }
+
+      if (Object.keys(updates).length) {
+        database.ref().update(updates);
+      }
     }
   });
 
 exports.onStoreWrite = functions.database.ref('/stores/{id}').onWrite(event => {
   const { params: { id } } = event;
-  const data = event.data.val();
-  if (!event.data.exists()) {
+  const eventSnapshot = event.data;
+  const data = eventSnapshot.val();
+  if (!eventSnapshot.exists()) {
     // on delete
-    const previous = event.data.previous.val();
+    const previous = eventSnapshot.previous.val();
 
     const menuGroupUpdates = Object.keys(previous.menuGroups).reduce((a, b) => {
       a[`/menuGroups/${b}`] = null;
@@ -76,11 +93,25 @@ exports.onStoreWrite = functions.database.ref('/stores/{id}').onWrite(event => {
     database.ref().update(Object.assign({}, menuGroupUpdates, menuUpdates));
   } else {
     // on create or update
-    const updates = {
-      [`/stores/${id}/menuCount`]: Object.keys(data.menus).length,
-      [`/stores/${id}/menuGroupCount`]: Object.keys(data.menuGroups).length,
-    };
-    database.ref().update(updates);
+    const updates = {};
+
+    if (!eventSnapshot.previous.exists()) {
+      updates[`/stores/${id}/menuGroups`] = data.menuGroups || {};
+      updates[`/stores/${id}/menus`] = data.menus || {};
+    }
+
+    if (eventSnapshot.child('menuGroups').changed()) {
+      updates[`/stores/${id}/menuGroupCount`] = Object.keys(
+        data.menuGroups
+      ).length;
+    }
+    if (eventSnapshot.child('menus').changed()) {
+      updates[`/stores/${id}/menuCount`] = Object.keys(data.menus).length;
+    }
+
+    if (Object.keys(updates).length) {
+      database.ref().update(updates);
+    }
   }
 });
 
