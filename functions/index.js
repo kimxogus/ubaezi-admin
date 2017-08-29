a151a9fd603ab6b3f0cf2880660a7d24d8763bf0
kimxogus <kgyoo8232@gmail.com>
2017-08-29 19:07:53 +0900

    add get-data script

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
 
+const database = admin.database();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
